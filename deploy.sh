4ad5389eb31d1b01443646132cbf89e3311571eb
Althafdev <151595814+Althafdev@users.noreply.github.com>
2024-03-13 23:18:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 # Login to Docker Hub
 docker login -u althafhub -p Althaf01@
-
+# Tag the Docker image with your Docker Hub username and repository name
+docker tag pythons althafhub/pyth:latest
 # Push the Docker image to Docker Hub
-docker push althafhub/pyth
+docker push althafhub/pyth:latest
 
